2d2ec5ddf0dba33b2323f74704a2a7209d99db20
AbismoNeo <luciogjj@gmail.com>
2022-05-12 23:51:22 -0500

    modificada la relación de listas de seguidores y agregados los verbose_name para el admin y registrados los modelos en el admin

--- sparrow_social/sparrow_app/admin.py
+++ sparrow_social/sparrow_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import users,message,list_follow
 
 # Register your models here.
+
+admin.site.register(users)
+admin.site.register(message)
+admin.site.register(list_follow)
--- sparrow_social/sparrow_app/models.py
+++ sparrow_social/sparrow_app/models.py
@@ -12,33 +12,44 @@ class users(models.Model):
     password = models.CharField(verbose_name = "Contraseña", max_length = 15)
     email = models.EmailField(verbose_name = "Correo Electronico")
     description  = models.TextField (verbose_name="Descripcion")
-    regdate = models.DateTimeField(default = timezone.now )
-    profilepic = models.ImageField()
+    regdate = models.DateTimeField(default = timezone.now, verbose_name="Fecha de Registro" )
+    profilepic = models.ImageField(verbose_name="Imagen de perfil")
     bgColor = models.CharField(verbose_name = "Color de Fondo", max_length = 15)
     txtColor = models.CharField(verbose_name = "Color de Texto", max_length= 15)
     
     def __str__(self):
         return f'Perfil de {self.username}'
     
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+    
 
 #Clase message con los datos de los post
 class message(models.Model):
-    datepost = models.DateTimeField(default = timezone.now)
-    user_id = models.ForeignKey(users, on_delete=models.CASCADE)
+    datepost = models.DateTimeField(default = timezone.now, verbose_name = "Fecha de posteo")
+    user_id = models.ForeignKey(users, on_delete=models.CASCADE, verbose_name="ID de usuario")
     Text = models.TextField(max_length=150, verbose_name = "Mensaje")
-    Image = models.ImageField()
+    Image = models.ImageField(verbose_name="Imagen")
 
     class Meta:   
         ordering = ['-datepost']
+        verbose_name_plural = 'Mensajes'
+        verbose_name = 'Mensaje'
 
     def __str__(self):
         return self.Text
 
 # Clase list_follow con los datos de lista de followers
 class list_follow(models.Model):
-    id_list = models.ForeignKey(users, on_delete=models.CASCADE)
-    id_friend = models.CharField(max_length=20)
-    follow_date = models.DateTimeField()
+    id_list = models.ForeignKey(users,related_name="Seguidor", on_delete=models.CASCADE, verbose_name="Id de Usuario")
+    id_friend = models.ForeignKey(users, related_name="Siguiendo", on_delete=models.CASCADE, verbose_name="Id de Seguidor")
+    follow_date = models.DateTimeField(verbose_name="Fecha de seguimiento")
 
     def __str__(self):
-        return self.id_friend
+        
+        return self.id_f
+    
+    class Meta:
+        verbose_name = 'Lista de Seguidores'
+        verbose_name_plural = 'Listas de Seguidores'
